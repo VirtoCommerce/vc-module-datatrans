cfe0b1141579c95f1cc053bf9ddc652c0e197a35
Oleg Zhuk <zhukoo@gmail.com>
2026-03-18 19:55:41 +0200

    fix: Fix capture operations (#26) (#27)
    Co-authored-by: Alexey Meshkov <alexeym@gmail.com>

--- src/VirtoCommerce.Datatrans.Core/Models/External/DatatransCaptureRequest.cs
+++ src/VirtoCommerce.Datatrans.Core/Models/External/DatatransCaptureRequest.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 namespace VirtoCommerce.Datatrans.Core.Models.External;
 
 public class DatatransCaptureRequest
@@ -5,4 +7,11 @@ public class DatatransCaptureRequest
     public long Amount { get; set; }
     public string Currency { get; set; }
     public string Refno { get; set; }
+
+    /// <summary>
+    /// Airline data. When this is a string containing JSON, it is serialized as a JSON object/array in the request body.
+    /// https://api-reference.datatrans.ch/#tag/v1transactions/operation/settle
+    /// </summary>
+    [JsonConverter(typeof(RawJsonConverter))]
+    public object AirlineData { get; set; }
 }
